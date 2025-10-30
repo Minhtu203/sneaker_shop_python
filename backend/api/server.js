647de52fdd33e804be4b0f2adc19823474161d48
Minhtu203 <minhtu09042003@gmail.com>
2025-10-30 17:20:06 +0700

    .

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,19 +1,28 @@
+import "dotenv/config";
 import express from "express";
-import serverless from "serverless-http";
 import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import connectDB from "../src/config/db.js";
+import cookieParser from "cookie-parser";
+import serverless from "serverless-http";
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT, credentials: true }));
+
+const CLIENT = process.env.CLIENT;
+app.use(
+  cors({
+    origin: CLIENT,
+    credentials: true,
+  })
+);
 
 connectDB();
 
-// routes
+// Import routes
 import authRoutes from "../src/routes/authRoutes.js";
 import userRoutes from "../src/routes/userRoutes.js";
 import shoesRoutes from "../src/routes/shoesRoutes.js";
