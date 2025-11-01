83bc34ab26f61bfe0bd9345e3a50d2695d9625f0
Minhtu203 <minhtu09042003@gmail.com>
2025-11-01 10:48:20 +0700

    .

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,23 @@ const redisClient = new Redis({
   port: process.env.REDIS_PORT || 6379,
   host: process.env.REDIS_HOST || "127.0.0.1",
   password: process.env.REDIS_PASSWORD,
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+  enableReadyCheck: true,
+
+  reconnectOnError: (err) => {
+    const targetErrors = ["READONLY", "CONN_HOST_ERROR", "EPIPE", "ETIMEDOUT"];
+    if (targetErrors.some((targetError) => err.message.includes(targetError))) {
+      return true;
+    }
+    return false;
+  },
+
   retryStrategy: (times) => {
-    const delay = Math.min(times * 50, 2000);
+    const delay = Math.min(times * 100, 2000);
     return delay;
   },
 });
